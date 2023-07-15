2f72f0132d69c0c3a2dac4c18290481bfcc69f57
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-15 15:34:40 +0530

    created base form

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,7 +9,7 @@ import (
 //Renders Template using html template
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
