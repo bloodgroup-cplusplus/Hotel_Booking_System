4557911c53534755c92f1f3e7ae46985db72a8c6
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-16 12:31:09 +0530

    adding better templating system

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,13 +3,16 @@ package render
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"path/filepath"
 )
 
 //Renders Template using html template
 
-func RenderTemplateTEst(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// create a template cache
+	// get the template (requested template from cache)
+	// render the template
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
@@ -18,6 +21,53 @@ func RenderTemplateTEst(w http.ResponseWriter, tmpl string) {
 	}
 
 }
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	// we are going to be returning a map of pointer string to template.tempalte
+	// we will create a variable
+
+	//myCache := make(map[string] *template.Template)
+
+	myCache := map[string]*template.Template{}
+	// we want to populate the cache with every available template
+	// all templates are on the root level of template
+
+	// get all of the files named *.page.tmpl from ./templates
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files ending with *.page.tmpl
+
+	for _, page := range pages {
+		// get the actual file name
+		// we want just the name of the template
+
+		name := filepath.Base(page) // last element of the path
+		// try to parse the file
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		// look for any layouts in the directory
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+		}
+	}
+
+}
+
+/* this is a naive way of doing it all the commented section by keeping the map and adding the name of the template everytime we use it
 
 var tc = make(map[string]*template.Template)
 
@@ -71,3 +121,4 @@ func createTemplateCache(t string) error {
 	return nil
 
 }
+*/
